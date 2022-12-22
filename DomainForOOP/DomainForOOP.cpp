5aee441f0e9ce2dfcff17ff8e3fd65727c47dc87
Александр Косилов <ted7007@yandex.ru>
2022-12-23 03:36:06 +1000

    feat: create client logic

--- DomainForOOP/DomainForOOP.cpp
+++ DomainForOOP/DomainForOOP.cpp
@@ -1,21 +1,27 @@
-// Насследование: дефолтный депозит, вип-депозит
-// Дружба:  ClientProfile, Deposit
-// Виртуальные функции чето с депозитом
-// Move semantics                     
+// inheritance - deposite+VIPDeposite, DefaultClient+Client
+// friendship - BankOperation+Deposite+Client
+// MoveSemantic - Client
 
 #include <iostream>
 #include <string>
 using namespace std;
 
+class BankOperation;
+
 
 class Deposite
 {
-public:
+    
+    
+protected:
+    friend class DepositeOperation;
     int _id;         // id of deposite
     int _profileId;  // id of client profile
     double _balance; // balance of deposite
     float _rate;     // percent rate of deposite
-
+    
+public:
+    
     Deposite(int id, int profileId, float rate)
     {
         _id = id;
@@ -53,13 +59,79 @@ public:
     
 };
 
+class DefaultClient{
+    
+protected:
+    int _id;
+    string _name;
+
+public:
+    DefaultClient(int id, string name)
+    {
+        _id = id;
+        _name = name;
+    }
+};
+
+class Client : DefaultClient
+{
+    friend class DepositeOperation;
+    
+public:
+    Deposite *_deposite;
+    
+    Client(int id, string name) : DefaultClient(id,name)
+    {
+        _deposite = new Deposite(id,id,3);
+    }
+    ~Client()
+    {
+        delete _deposite;
+    }
+    Client(Client &&clientForMove) : DefaultClient(clientForMove._id, clientForMove._name)
+    {
+        this->_deposite = clientForMove._deposite;
+        clientForMove._deposite = nullptr;
+    }
+};
+
+static class DepositeOperation
+{
+public:
+    static void PutOnDeposite(Client &client, double sum)
+    {
+        client._deposite->_balance+=sum;
+    }
+    
+    // return true if operation is success
+    static bool WithdrawFromDeposite(Client &client, double sum)
+    {
+        if((client._deposite->_balance-sum) < 0)
+            return false;
+        client._deposite->_balance-=sum;
+        return true;
+    }
+};
+
+
+
+Client CreateClient()
+{
+    Client client(1,"Client");
+    return client;
+}
 
 int main(int argc, char* argv[])
 {
-    Deposite dep1(2,5,7);
-    VIPDeposite dep2(1,2,3);
-    dep1.Display();
-    dep2.Display();
+    Deposite defaultDep(12,2,3);
+    VIPDeposite vip(4,5,6);
     
+    Client user(13, "Alex");
+
+    DepositeOperation::PutOnDeposite(user, 100);
+    DepositeOperation::WithdrawFromDeposite(user, 5);
+    user._deposite->Display();
+    Client user2(CreateClient());
+    user2._deposite->Display();
     return 0;
 }
