b47b5dc3262d3b87af6d78c247778df223353599
Kosilov Aleksandr <Kosilov.AA@dns-shop.ru>
2023-03-18 01:13:52 +1000

    realize container

---
+++ Container/Element/User.cs
@@ -0,0 +1,29 @@
+﻿namespace Container.Element;
+
+public class User
+{
+    public string Name { get; set; }
+
+    public string Login { get; set; }
+
+    public string Password { get; set; }
+    
+    public override bool Equals(object obj) => this.Equals(obj as User);
+
+    public bool Equals(User? user)
+    {
+        if (user is null)
+            return false;
+        if (object.ReferenceEquals(this, user))
+            return true;
+        
+        if (this.GetType() != user.GetType())
+        {
+            return false;
+        }
+
+        return user.Name == this.Name
+               && user.Login == this.Login
+               && user.Password == this.Password;
+    }
+}
